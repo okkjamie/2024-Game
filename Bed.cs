7e2fbcb497567996f96f7eb160cc2516c034885e
okkjamie <161522522+okkjamie@users.noreply.github.com>
2024-03-25 14:25:53 +1300

    added sleep cutscene

--- Bed.cs
+++ Bed.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Bed : MonoBehaviour
 {
+    public GameObject Alarm;
+    public GameObject cutscene;
+    public PlayerActions playerActions;
+    public PlayerMovement playerMovement;
+    public HeadbobSystem headbob;
+
+    public AudioSource alarmSource;
+    public AudioClip clip;
+
+    bool canSleep;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canSleep = true;
     }
 
     // Update is called once per frame
@@ -18,6 +28,39 @@ public class Bed : MonoBehaviour
 
     public void Sleep()
     {
+        StartCoroutine(SleepCutscene());
+    }
+
+    IEnumerator SleepCutscene()
+    {
+        if(canSleep)
+        {
+            canSleep = false;
+            TimeManager time = Alarm.GetComponent(typeof(TimeManager)) as TimeManager;
+            time.AnomolyCheck();
+            Debug.Log("Sleep!");
+            cutscene.SetActive(true);
+            playerMovement.enabled = !playerMovement.enabled;
+            playerActions.enabled = !playerActions.enabled;
+            headbob.enabled = !headbob.enabled;
+            yield return new WaitForSeconds(5f);
+            playerMovement.enabled = !playerMovement.enabled;
+            playerActions.enabled = !playerActions.enabled;
+            headbob.enabled = !headbob.enabled;
+            cutscene.SetActive(false);
+            Debug.Log("Wake!");
+
+            alarmSource.clip = clip;
+            alarmSource.volume = Random.Range(0.35f, 0.65f);
+            alarmSource.pitch = Random.Range(0.8f, 1.2f);
+            alarmSource.Play();
+            yield return new WaitForSeconds(5f);
+            alarmSource.Stop();
+
+            yield return new WaitForSeconds(7f);
+            canSleep = true;
+        }
         
+
     }
 }
