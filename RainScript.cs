ed0a01bd8903b4492322bfb1774fd8c764789f3c
okkjamie <161522522+okkjamie@users.noreply.github.com>
2024-05-01 12:46:47 +1200

    Update RainScript.cs
    Formatted the code

--- RainScript.cs
+++ RainScript.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class RainScript : MonoBehaviour
 {
-    public AudioSource thunderSource;
-    public AudioClip clip;
-    // Start is called before the first frame update
+    [Header("Rain Sources")]
+    public AudioSource thunderSource; // The source of the thunder (for 3D Reverb)
+    public AudioClip clip; // The thunder sound effect
+
     void Start()
     {
-        StartCoroutine(FootstepSounds());
+        StartCoroutine(FootstepSounds()); // Starts the repeating sound 
     }
 
     IEnumerator FootstepSounds()
     {
-         thunderSource.clip = clip;
-            thunderSource.volume = Random.Range(0.65f, 0.85f);
-            thunderSource.pitch = Random.Range(0.8f, 1.2f);
-            thunderSource.Play();
-            yield return new WaitForSeconds(Random.Range(20, 45));
-            StartCoroutine(FootstepSounds());
-  
+        thunderSource.clip = clip; // Specifys Clip
+        thunderSource.volume = Random.Range(0.65f, 0.85f); // Randomize volume
+        thunderSource.pitch = Random.Range(0.8f, 1.2f); // Randomize pitch (how deep or high it sounds)
+        thunderSource.Play(); // Plays the audio source
+
+        yield return new WaitForSeconds(Random.Range(20, 45)); // Waits a random time then plays
+        StartCoroutine(FootstepSounds()); // Repeats the thunder sound effect
     }
 }
